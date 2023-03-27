477e743cf39cc0529cb56799c89045cce30244fb
Syirhel <SVCJ012@gmail.com>
2023-03-28 02:24:07 +0800

    Added Vector Assets and Image Buttons

--- app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/fragments/adapters/FragmentPageAdapter.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/fragments/adapters/FragmentPageAdapter.kt
@@ -7,14 +7,14 @@ import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import ph.edu.dlsu.mobdeve.mojicajera.itipid.fragments.*
 
-class FragmentPageAdapter(
-    fragmentManager: FragmentManager,
-    lifecycle: Lifecycle
-) : FragmentStateAdapter(fragmentManager,lifecycle){
+class FragmentPageAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager,lifecycle){
+
+    // Number of Tabs Displayed on The Upper Nav Tab Layout
     override fun getItemCount(): Int {
         return 5
     }
 
+    // Sets The Order of The Tab Fragments
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> DailyFragment()
