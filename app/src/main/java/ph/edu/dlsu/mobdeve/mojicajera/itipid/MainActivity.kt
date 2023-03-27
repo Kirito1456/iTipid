568fe9d706fcfee1c50c3dbe1f7a629249fe4d82
Syirhel <SVCJ012@gmail.com>
2023-03-27 20:25:03 +0800

    Connected Create Account Text View

--- app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/MainActivity.kt
@@ -1,11 +1,37 @@
 package ph.edu.dlsu.mobdeve.mojicajera.itipid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        //This Segments Handles Create Account and Trouble Logging In Text View//
+
+        // On Click Listener for Create an Account Text View
+        val registerTV = findViewById<TextView>(R.id.createAccount)
+        registerTV.setOnClickListener(){
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+
     }
+
+
+
+
+
+
 }
