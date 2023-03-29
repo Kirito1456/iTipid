31471d33a78a8123a5b4c86d926d5b90e49742dc
Syirhel <SVCJ012@gmail.com>
2023-03-29 23:48:49 +0800

    Add Balance Activity

--- app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/AddBalance.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/AddBalance.kt
@@ -2,8 +2,26 @@ package ph.edu.dlsu.mobdeve.mojicajera.itipid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.*
+
+
 
 class AddBalance : AppCompatActivity() {
+
+    //EditTextViews
+    private lateinit var etTransacName: EditText
+    private lateinit var etTransacAmount: EditText
+    private lateinit var etTransacDate: EditText
+    private lateinit var etTransacDescription: EditText
+    //Buttons
+    private lateinit var saveButton: Button
+    private lateinit var cancelButton: Button
+
+    //Database Reference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_balance)
--- app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/dataclass/User.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/dataclass/User.kt
@@ -1,6 +1,7 @@
 package ph.edu.dlsu.mobdeve.mojicajera.itipid.dataclass
 
 class User(
+    var UID : String,
     var Name : String,
     var Username: String,
     var Password: String,
