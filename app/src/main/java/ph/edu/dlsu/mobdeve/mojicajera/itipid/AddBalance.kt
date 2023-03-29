9d0cd777379da50ea2ff8a0212a661df3be250a7
Syirhel <SVCJ012@gmail.com>
2023-03-29 21:21:52 +0800

    With Add Balance View

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/AddBalance.kt
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.mobdeve.mojicajera.itipid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBalance : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_balance)
+
+
+    }
+}
