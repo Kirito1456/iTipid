a5159ba46b3b76c366282b7aca858699138bbc61
Syirhel <SVCJ012@gmail.com>
2023-04-07 23:45:26 +0800

    Calendar stores Date Primary Key (NON API)

--- app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/fragments/CalendarFragment.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/mojicajera/itipid/fragments/CalendarFragment.kt
@@ -5,20 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
+import android.widget.Toast
 import ph.edu.dlsu.mobdeve.mojicajera.itipid.R
-
+import java.text.DateFormatSymbols
 
 
 class CalendarFragment : Fragment() {
 
-
+    private lateinit var dateSet: String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+        var view = inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        val calendarView = view.findViewById<CalendarView>(R.id.calendarView)
+        val textView = view.findViewById<TextView>(R.id.events)
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            dateSet = String.format("%02d/%02d/%04d", month+1, dayOfMonth, year)
+            var date = convertDateFormat(dateSet)
+            textView.text = "Events for: $date"
+        }
+        return view
     }
-
-
 }
+
+fun convertDateFormat(inputDate: String): String {
+    val dateParts = inputDate.split("/")
+    val month = dateParts[0].toInt()
+    val day = dateParts[1].toInt()
+    val year = dateParts[2]
+
+    val monthString = DateFormatSymbols().months[month - 1]
+    return "$year, $monthString $day"
+}
+
